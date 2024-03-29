7b0cc98bf5ce72fc5cb00751ed7d47403a74006d
Sergey Poluyan <svpoluyan@gmail.com>
2024-03-29 03:44:40 +0300

    up

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+import std;
+
+import oper;
+import calc;
+
+int main()
+{
+	std::cout << calc(3, 4, "add") << std::endl;
+	std::cout << add(3, 4) << std::endl;
+}
---
+++ src/oper.ixx
@@ -0,0 +1,6 @@
+export module oper;
+
+import std;
+
+export int add(int a, int b) { return a + b; }
+export int sub(int a, int b) { return a - b; }
