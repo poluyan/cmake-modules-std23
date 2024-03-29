d02cc9e3e58f4dfe5b913ea2fadb76644890b97f
Sergey Poluyan <svpoluyan@gmail.com>
2024-03-29 05:46:49 +0300

    up

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if test -d ./build; then
-  rm -rf build
-fi
-#add_compile_options(-fmodules -fbuiltin-module-map)
-export CFLAGS="-stdlib=libc++ -fmodules -fbuiltin-module-map"
-cmake -G Ninja -B build -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_CXX_FLAGS:STRING="$CFLAGS" -DCMAKE_BUILD_TYPE=Release
-ninja -C build
-./build/calculator
